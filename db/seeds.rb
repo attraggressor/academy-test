a1de65ee12361ceb0f414d806bd9b977b12827e2
Dominik Spisak <dominik.spisak@domena.megiteam.pl>
2015-02-28 18:41:28 +0100

    added cities to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+City.create!(name: 'Radom', short_name: 'Ra')
+City.create!(name: 'Sosnowiec', short_name: 'Ni')
+City.create!(name: 'Krakow', short_name: 'Kr')
+City.create!(name: 'Poznan', short_name: 'Po')
+City.create!(name: 'Warszawa', short_name: 'Wm')
+City.create!(name: 'Gdansk', short_name: 'Gd')
+City.create!(name: 'Katowice', short_name: 'Kb')
