228111246017e30365204f420e964add3c4b3946
Dominik Spisak <dominik.spisak@domena.megiteam.pl>
2015-02-28 18:04:29 +0100

    added city_id to user

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :age, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :age, :email, :city_id, :password, :password_confirmation, :current_password)
   end
 end
